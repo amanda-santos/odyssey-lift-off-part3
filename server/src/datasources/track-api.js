f17b3113a26477738af965872accda7e0736b707
Amanda Santos <amanda.santos@pipefy.com>
2022-02-07 20:44:21 -0300

    ✨ Start adding new track endpoint

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,14 +1,22 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class TrackAPI extends RESTDataSource {
   constructor() {
     super();
     // the Catstronauts catalog is hosted on this server
-    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+    this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
   }
 
   getTracksForHome() {
-    return this.get('tracks');
+    return this.get("tracks");
+  }
+
+  getTrack(trackId) {
+    return this.get(`track/${trackId}`);
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
   }
 
   getAuthor(authorId) {
